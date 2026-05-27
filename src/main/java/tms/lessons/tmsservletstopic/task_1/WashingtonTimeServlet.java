33483f2f51067301c85b7499b8d25670aa619021
ilona <ilonka.kulik@bk.ru>
2026-05-27 16:57:26 +0300

    Task 1 is completed

---
+++ src/main/java/tms/lessons/tmsservletstopic/task_1/WashingtonTimeServlet.java
@@ -0,0 +1,32 @@
+package tms.lessons.tmsservletstopic.task_1;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@WebServlet("/washington")
+public class WashingtonTimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("HH:mm:ss");
+        //Time in Washington = Time in New_York
+        ZonedDateTime washingtonTime = ZonedDateTime.now(ZoneId.of("America/New_York"));
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1> Время в Вашингтоне "  + washingtonTime.format(fmt) + "</h1>");
+        out.println("</body></html>");
+
+    }
+}
